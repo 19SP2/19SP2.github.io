b49982ebabe5a3d37ee071d7630b5710619930d5
Sharvari Sunil Pradhan <113926050+19SP@users.noreply.github.com>
2024-01-06 21:18:27 +0400

    5

---
+++ script_file.js
@@ -0,0 +1,5 @@
+function show(){
+    document.querySelector('.discover').classList.toggle('open') /*toggle helps to check the visibility of elements*/
+    document.querySelector('.navigation').classList.toggle('active')
+    /*hamburger menu by CodeMode available at https://www.youtube.com/watch?v=vkNkeCSb13M*/
+}
